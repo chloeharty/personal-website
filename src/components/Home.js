580d311bff980176204706e76438918096bc4c9c
chloeharty <chloe.c.harty@gmail.com>
2025-11-01 15:43:16 -0700

    Adding Package.json

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div>
+            <h1>I'm Chloe</h1>
+            <h2>Welcome to My Website</h2>
+            <p>
+                My name is Chloe Harty. I am a second-year Software Engineering student at the University of Victoria (UVic).
+                I grew up in Sherwood, Oregon where I thoroughly enjoyed my time at Sherwood High School and I graduated with 
+                an Advanced Honors Diploma in 2023. My extended family resides in Southern Alberta and I’ve spent many summers 
+                in remote corners of the prairies. UVic was the perfect middle ground between my two homes. I chose to pursue 
+                engineering because I have a long-standing passion for design, creativity, and the pursuit of innovation. The 
+                exceptional caliber of education and the software engineering specific program solidified my decision to attend 
+                UVic. Explore my website to discover my academic clubs, projects, and the early creative designs that motivated 
+                me to become an engineer.
+            </p>
+        </div>
+    );
+};
+
+export default Home;
